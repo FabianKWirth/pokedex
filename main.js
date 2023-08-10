f7b56e03d7f715287801ee07c811b484549451b6
FabianKWirth <135059007+FabianKWirth@users.noreply.github.com>
2023-08-10 02:15:27 +0200

    Changed styles and functionality, added bootstrap

--- main.js
+++ main.js
@@ -1,38 +1,10 @@
 let currentPokemon = null;
-let pokemons = [
-    "bulbasaur",
-    "charmander",
-    "squirtle",
-    "pikachu",
-    "jigglypuff",
-    "meowth",
-    "psyduck",
-    "geodude",
-    "gengar",
-    "kangaskhan",
-    "mr-mime",
-    "eevee",
-    "ditto",
-    "vaporeon",
-    "jolteon",
-    "flareon",
-    "porygon",
-    "dragonite",
-    "mewtwo",
-    "mew"
-];
+let pokemonAmount=25;
 
-/*
-  pokemons=[
-    "makuhita",
-    "hariyama",
-    "squirtle"
-  ];
-*/
 
 async function init() {
     if (currentPokemon == null) {
-        await getPokemonOverview();
+        await renderPokemonGrid();
     } else {
         await renderCurrentPokemon();
     }
@@ -44,11 +16,64 @@ async function renderCurrentPokemon() {
 
 
 
-async function getPokemonOverview() {
+async function renderPokemonGrid() {
     emptyContainer();
-    for (let index = 0; index < pokemons.length; index++) {
-        await loadSimplePokemon(index);
+    for (let index = 0; index < pokemonAmount; index++) {
+        await renderPokemonGridItemLayout(index);
+        await loadPokemonGridValues(index);
     }
+}
+
+
+function renderPokemonGridItemLayout(index) {
+    html=`<div id="pokemon${index}" class="pokemon-grid-container">`;
+    html+=`<div class='d-flex justify-content-between align-items-baseline w-75'>
+        <h2 id='pokemonName${index}'></h2>
+        <div id='pokemonId${index}' class='align-bottom'></div>
+    </div>`;
+    html+=
+    `<div id='pokemonContent${index}' class='d-flex justify-content-between  w-75'>
+        <div class='btn-group-vertical'id='pokemonTypes${index}'></div>
+        <img id='pokemonImg${index}'>
+    </div>`;   
+    
+    document.getElementById("pokedex").innerHTML += html;
+}
+
+async function loadPokemonGridValues(index) {
+
+    let pokemonData = await getPokemonData(index);
+    if(pokemonData[0]){
+        const currentPokemonData=pokemonData[1];
+    let defaultImg = currentPokemonData["sprites"]["front_default"];
+    document.getElementById("pokemonImg" + index).src = defaultImg;
+
+    let name = currentPokemonData["name"];
+    document.getElementById("pokemonName" + index).innerHTML = name;
+
+    let id = index+1;
+    document.getElementById("pokemonId" + index).innerHTML = '#'+id;
+
+    let types = currentPokemonData["types"];
+    document.getElementById("pokemonTypes" + index).innerHTML = "";
+    for (let i = 0; i < types.length; i++) {
+        document.getElementById("pokemonTypes" + index).innerHTML += 
+        `<button class='btn type-button d-inline-block ${types[i]['type']['name']}' disabeld>${types[i]['type']['name']}</button>`;
+    }
+    }
+}
+
+async function getPokemonData(index) {
+    pokemonId=index+1;//pokemonId startet bei 1; index bei 0; 
+    try {
+        let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
+        let response = await fetch(url);
+        let pokemonData = await response.json();
+        return ['true',pokemonData];
+    } catch (e) {
+        return ['false',pokemonData];
+    }
+
 }
 
 function emptyContainer() {
@@ -60,51 +85,11 @@ function unsetCurrentPokemon() {
 }
 
 
-async function loadSimplePokemon(index) {
-    pokemonName = pokemons[index];
-    //pokemonData = await getCurrentPokemonValues();
-    try {
-        let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonName;
-        let response = await fetch(url);
-        let pokemonData = await response.json();
-        console.log(pokemonData);
-
-        renderPokemonLayout(index,"extended");
-        renderHeaderPokemonValues(index, pokemonData);
-    } catch (e) {
-        console.error(e);
-    }
-
-}
-
-function renderHeaderPokemonValues(index, pokemonData) {
-    let defaultImg = pokemonData["sprites"]["front_default"];
-    document.getElementById("pokemonImg" + index).src = defaultImg;
-
-    let name = pokemonData["name"];
-    document.getElementById("pokemonName" + index).innerHTML = name;
-
-    let types = pokemonData["types"];
-    document.getElementById("pokemonTypes" + index).innerHTML = "";
-
-    for (let i = 0; i < types.length; i++) {
-        document.getElementById("pokemonTypes" + index).innerHTML += `<div class='pokemon-type ${types[i]['type']['name']}'>`+types[i]['type']['name']+`</div>`;
-    }
-}
 
 
-function renderPokemonLayout(index,type) {
-    html=`<div id="pokemon${index}" class="pokemon-container">`
-    if(type=='simple'){
-        html+=getPokemonHeaderLayout(index)+`</div>`;
-    }else{
-        html+=getPokemonHeaderLayout(index)+getPokemonBodyLayout(index);
-    }
 
-    html+=`</div>`;
-    
-    document.getElementById("pokedex").innerHTML += html;
-}
+
+
 
 function getPokemonBodyLayout(index) {
     html = `<div class='pokemon-info-container'></div>`;
